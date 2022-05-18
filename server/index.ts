d8a3f7b740732b29075960ef4e49747cf456c7a3
Olivia isberg <oliviaisberg@gmail.com>
2022-05-18 13:58:48 +0200

    Changed connection to database from env file

--- server/index.ts
+++ server/index.ts
@@ -1,24 +1,23 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-
-dotenv.config();
+import * as dotenv from 'dotenv';
+import { env } from 'process';
+require('dotenv').config();
 
 const PORT = 5500;
 const app = express();
 
 app.use(express.json());
 
-// mongoDB connection-string
-const MONGO_URI =
-  'mongodb+srv://grupp1:12345@cluster1.z3nab.mongodb.net/?retryWrites=true&w=majority';
-
-try {
-  // Connect to the MongoDB cluster
-  mongoose.connect(MONGO_URI, () => console.log(' Mongoose is connected'));
-} catch (e) {
-  console.log('could not connect');
-}
+//Connect to mongoose
+dotenv.config({ path: '/.env' });
+mongoose.connect(
+  `${process.env.DB_CONNECTION}`,
+  {
+    dbName: 'kameraproffset',
+  },
+  () => console.log('Connected to mongoDB')
+);
 
 //Server is running
 app.listen(PORT, () => {
