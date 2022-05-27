7b7d1d8b07a3fbc00582478f7956fa478df62069
Olivia isberg <oliviaisberg@gmail.com>
2022-05-27 11:40:52 +0200

    Changed name on file to uppercase

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter } from 'react-router-dom';
 import ShoppingCartProvider from '../contexts/ShoppingCartContext';
 import Layout from './Layout';
-import ConfirmationProvider from '../contexts/confirmationContext';
+import ConfirmationProvider from '../contexts/ConfirmationContext';
 import UserProvider from '../contexts/UserContext';
 import FilterCategoryContext from '../contexts/FilterCategoriesContext';
 
