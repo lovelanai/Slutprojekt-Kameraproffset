7c524571e38f84e76acbab4394f1356c99002409
Olivia isberg <oliviaisberg@gmail.com>
2022-06-02 09:32:36 +0200

    Added import for cookiesession

--- server/errorFunctions.ts
+++ server/errorFunctions.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import 'cookie-session';
 
 export function assertIsLoggedIn(req: Request, res: Response, message: string) {
   if (req.session?.user === undefined) {
